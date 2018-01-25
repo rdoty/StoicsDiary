9510edd5dd3d0a779cfcdb5f08d1777f7ac4901b
Rick Doty <rick.a.doty@gmail.com>
2018-01-25 13:03:34 -0800

    - Database changes - More cleanup

--- mobile/src/main/java/com/appollonius/stoicsdiary/StoicActivity.java
+++ mobile/src/main/java/com/appollonius/stoicsdiary/StoicActivity.java
@@ -21,6 +21,7 @@ public class StoicActivity extends AppCompatActivity implements ChoiceFragment.O
     private StoicDatabase db;
     static final String TABLE_BASE = "diary";
     static final String TABLE_DESC = "feels";
+    static final String COLUMN_DAY = "time_stamp";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,7 +43,7 @@ public class StoicActivity extends AppCompatActivity implements ChoiceFragment.O
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);  // TODO Auto-generated method stub
+        super.onSaveInstanceState(outState);  // Auto-generated method stub
     }
 
     @Override
@@ -92,10 +93,10 @@ public class StoicActivity extends AppCompatActivity implements ChoiceFragment.O
      * @return Boolean true, false or NULL
      */
     Boolean getDayValue(Long date) {
-        String Q_SELECTVALUE = "SELECT value from diary WHERE timestamp=%s;";
+        String Q_SELECTVALUE = "SELECT value from diary WHERE %s=%s;";
         SQLiteDatabase dbr = db.getReadableDatabase();
 
-        Cursor cursor = dbr.rawQuery(String.format(Q_SELECTVALUE, date), null);
+        Cursor cursor = dbr.rawQuery(String.format(Q_SELECTVALUE, COLUMN_DAY, date), null);
         Boolean retVal = cursor.moveToFirst() ? 1 == cursor.getInt(0) : null;
 
         cursor.close();
@@ -103,7 +104,7 @@ public class StoicActivity extends AppCompatActivity implements ChoiceFragment.O
     }
 
     /**
-     * TODO Find an elegant upsert behavior, sqlite has 'INSERT OR REPLACE'
+     * Would like to find a more elegant upsert behavior, sqlite has 'INSERT OR REPLACE'
      * @param date Long from LocalDate
      * @param newValue Boolean value to assign
      * @return Boolean whether the set was a success
@@ -111,13 +112,14 @@ public class StoicActivity extends AppCompatActivity implements ChoiceFragment.O
     Boolean setDayValue(Long date, Boolean newValue) {
         String logString = "Setting long date %s to value %s, was %s";
         SQLiteDatabase dbw = db.getWritableDatabase();
+        Boolean originalValue = getDayValue(date);
         ContentValues dbValue = new ContentValues();
         dbValue.put("value", newValue);
-        Boolean originalValue = getDayValue(date);
+
         if (originalValue != null) {  // Update
-            dbw.update(StoicActivity.TABLE_BASE, dbValue, "timestamp=" + date, null);
+            dbw.update(StoicActivity.TABLE_BASE, dbValue, String.format("%s=%s", COLUMN_DAY, Long.toString(date)), null);
         } else {  // Insert
-            dbValue.put("timestamp", date);
+            dbValue.put(COLUMN_DAY, date);
             dbw.insert(StoicActivity.TABLE_BASE, null, dbValue);
         }
         Log.d("DateSet", String.format(logString, date, newValue, originalValue));
@@ -126,13 +128,12 @@ public class StoicActivity extends AppCompatActivity implements ChoiceFragment.O
     }
 
     /**
-     * TODO: Earliest recorded date in DB
      * @return Long Date
      */
     long getEarliestEntryDate() {
         Long retVal;
         SQLiteDatabase dbr = db.getReadableDatabase();
-        Cursor c = dbr.query(StoicActivity.TABLE_BASE, new String[] { "min(timestamp)" },
+        Cursor c = dbr.query(StoicActivity.TABLE_BASE, new String[] { String.format("min(%s)", COLUMN_DAY) },
                 null, null,null, null, null);
         c.moveToFirst();
         retVal = c.getLong(0);
@@ -140,24 +141,14 @@ public class StoicActivity extends AppCompatActivity implements ChoiceFragment.O
         return retVal * 86400;  // 24 * 60 * 60
     }
 
-    /**
-     * This clears out the DB
-     */
-    void truncateTables() {
-        SQLiteDatabase dbw = db.getWritableDatabase();
-        String Q_TABLETRUNC = "DELETE FROM %s; DELETE FROM SQLITE_SEQUENCE WHERE name='%s';";
-        dbw.execSQL(String.format(Q_TABLETRUNC, TABLE_DESC, TABLE_DESC));
-        dbw.execSQL(String.format(Q_TABLETRUNC, TABLE_BASE, TABLE_BASE));
-    }
-
-    /**
-     *
+    /*
+     * These clear out the DB
      */
     void rebuildDatabase() {
         SQLiteDatabase dbw = db.getWritableDatabase();
         String Q_TABLE_DROP = "DROP TABLE IF EXISTS %s;";
         String Q_TABLE_MAKE = "CREATE TABLE %s (%s);";
-        String COLUMNS_BASE = "id INTEGER PRIMARY KEY, time_stamp DATE UNIQUE, last_updated DATE, update_count TINYINT, value BOOLEAN";
+        String COLUMNS_BASE = "id INTEGER PRIMARY KEY, "+COLUMN_DAY+" DATE UNIQUE, last_updated DATE, update_count TINYINT, value BOOLEAN";
         String COLUMNS_DESC = "id INTEGER PRIMARY KEY, diary_id INTEGER, words VARCHAR(255), FOREIGN KEY (diary_id) REFERENCES %s(id)";
 
         dbw.execSQL(String.format(Q_TABLE_DROP, TABLE_BASE));
@@ -167,10 +158,13 @@ public class StoicActivity extends AppCompatActivity implements ChoiceFragment.O
     }
 
     /*
-     * END Database accessors
-     */
+    void truncateTables() {
+        SQLiteDatabase dbw = db.getWritableDatabase();
+        String Q_TABLETRUNC = "DELETE FROM %s; DELETE FROM SQLITE_SEQUENCE WHERE name='%s';";
+        dbw.execSQL(String.format(Q_TABLETRUNC, TABLE_DESC, TABLE_DESC));
+        dbw.execSQL(String.format(Q_TABLETRUNC, TABLE_BASE, TABLE_BASE));
+    }
 
-    /*
     private void dropAllUserTables(SQLiteDatabase db) {
         Cursor cursor = db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
         //noinspection TryFinallyCanBeTryWithResources not available with API < 19
@@ -193,4 +187,8 @@ public class StoicActivity extends AppCompatActivity implements ChoiceFragment.O
         }
     }
     */
+
+    /*
+     * END Database accessors
+     */
 }
